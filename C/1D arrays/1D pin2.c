3f36a5cfbcbde40cc4bcb53598e176aab98ccf37
Drosos Giannhs <giannhsdrosos77@gmail.com>
2026-01-11 13:12:18 +0200

    Update 1Darray2.c

--- C/1D arrays/1D pin2.c
+++ C/1D arrays/1D pin2.c
@@ -1,23 +1,23 @@
-//ginomeno me pinaka
+//product of 1D static array
 #include <stdio.h>
 #define N 5
 main()
 {
-   int A[N],i,gin;
-   	gin=1;	
+   int A[N],i,prod;
+   	prod=1;	
 	for (i=0;i<N;i++)
 	{
-		printf("Dose akeraio gia thn %d thesi tou pinaka 1-8 :",i+1);
-		scanf("%d",&A[i]);
-		if(A[i]<1||A[i]>8){
-		
-       	   do{   
-		    printf("Lathos dose timh 1-8,janadose :");
-       		scanf("%d",&A[i]);
+	  	printf("Give an integer between 1-8 for %d     content of the array :",i+1);
+	 	scanf("%d",&A[i]);
+		 if(A[i]<1||A[i]>8){
+      do{   
+		        printf("Invalid value,try again.");
+       	 	scanf("%d",&A[i]);
              
-		   }while(A[i]<1||A[i]>8);     
-	     }
-		   gin*=A[i];
+		     }while(A[i]<1||A[i]>8);     
+	    }
+
+		   prod*=A[i];
      }
-     printf("To ginomeno einai:%d",gin);
+     printf("Array's product is:%d",prod);
 }
